ccdc827db8ba1a74747f62716b64a87eb3a2537f
Charly Bernard <charly.bernard@gmail.com>
2025-07-24 23:41:00 +0200

    Set up the site shell and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
---
+++ src/components/number/number.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import NumberFlow from '@number-flow/vue';
+  import { ref } from 'vue';
+
+  const props = defineProps({
+    value: {
+      type: Number,
+      default: 0,
+      required: true,
+    },
+    trend: {
+      type: Number,
+      default: 0,
+    },
+    format: {
+      type: Object,
+      default: () => ({ notation: 'standard' }),
+    },
+  });
+
+  const digit = ref(props.value);
+
+  const handleClick = () => {
+    digit.value = digit.value + 100;
+  };
+</script>
+
+<template>
+  <NumberFlow :value="digit" :trend @click="handleClick" />
+</template>
